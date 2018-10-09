75f227fa10739108a2ed31738308da1225338b69
berhalak <jaroslaw.sadzinski@gmail.com>
2018-10-10 00:20:01 +0200

    Adding auto computed properties

--- component.ts
+++ component.ts
@@ -1,19 +1,21 @@
 import Vue from "vue";
 import VueRouter, { Route } from "vue-router";
 
-
+/** Decorates field as vue prop */
 export function prop(target: Object, propertyKey: string | symbol) {
 	let s = <any>target;
 	s.props = s.props || [];
 	s.props.push(propertyKey);
 }
 
+/** Defines computed property, deprected all getters and setters are computed */
 export function computed(target: Object, propertyKey: string | symbol) {
 	let s = <any>target;
 	s.computed = s.computed || [];
 	s.computed.push({ target, propertyKey });
 }
 
+/** Declares method as watch handler with options: deep, imidiate */
 export function watch(...name: string[]) {
 	return function (target: Object, propertyKey: string | symbol) {
 		let s = <any>target;
@@ -26,8 +28,8 @@ export type Constructor<T> = {
 	new(...args: any[]): T;
 }
 
+/** Base class for vue components in typescript */
 export class Component {
-
 	protected $emit!: (name: string, payload?: any) => void;
 	protected $set!: (obj: any, propKey: string, value: any) => void;
 	protected $route!: Route;
@@ -36,15 +38,6 @@ export class Component {
 	protected $value: any;
 	protected $nextTick!: ((clb: () => void) => void | Promise<any>);
 	protected $refs: any;
-
-	protected on!: (name: string, handler: any) => void;
-	protected emit!: (name: string, payload: any) => void;
-
-	protected track() {
-
-	}
-
-
 	protected created() {
 
 	}
@@ -64,49 +57,25 @@ export class Component {
 	protected mounted() {
 
 	}
-
-	protected getParams() {
-		let self = <Vue><any>this;
-		return self.$route.params;
-	}
 }
 
-export class ValueComponent<T> extends Component {
-
-	@prop
-	public value!: T;
-
-	protected track() {
-
-	}
-
-	protected commit(value: T) {
-		this.$value = value;
-	}
-
-	@watch("value")
-	protected populate() {
-
-	}
-}
-
-
-function getPropertyDesc(proto : any, name : string) {
-	while (proto) {
-		let desc = Object.getOwnPropertyDescriptor(proto, name);
-		if (desc) return desc;
-		proto = Object.getPrototypeOf(proto);
-	}
-}
-
+/** Internal function for changing class based declarations to proper vue component */
 export function bootstrap(klass: Constructor<Component>) {
+	function getPropertyDesc(proto: any, name: string) {
+		while (proto) {
+			let desc = Object.getOwnPropertyDescriptor(proto, name);
+			if (desc) return desc;
+			proto = Object.getPrototypeOf(proto);
+		}
+	}
+
 	let name = klass.prototype.name;
 	let proper: any = {};
 	let anyKlass = klass as any;
-	let propsNames : any = [];
+	let propsNames: any = [];
 	if (klass.prototype.props && klass.prototype.props.length) {
 		// create class for default values
-		let defData : any = new klass();
+		let defData: any = new klass();
 		proper.props = {};
 		for (let p of klass.prototype.props) {
 			propsNames.push(p);
@@ -145,19 +114,32 @@ export function bootstrap(klass: Constructor<Component>) {
 	let proto = klass.prototype;
 	proper.components = anyKlass.prototype.constructor.components;
 
-	let methods : any = {};
+	let methods: any = {};
 	let excluded = ['constructor', 'created', 'mounted', 'beforeCreate', 'updated', 'beforeUpdate'];
 	if (klass.prototype.watches) {
-		excluded = excluded.concat(klass.prototype.watches.map((x : any) => x.propertyKey));
+		excluded = excluded.concat(klass.prototype.watches.map((x: any) => x.propertyKey));
 	}
 
 	for (let m in klass.prototype) {
 		let protoStart = proto;
 		let desc = getPropertyDesc(proto, m);
-		if (desc && desc.get)
-			continue;
+		if (desc !== undefined && desc.get !== undefined) {
+			proper.computed = proper.computed || {};
+			proper.computed[m] = {
+				get() {
+					if (desc !== undefined && desc.get !== undefined)
+						return desc.get.call(this);
+				},
+				set(value: any) {				
+					if (desc !== undefined && desc.set !== undefined) {
+						desc.set.call(this, value);
+					} else {
+						throw 'Property ' + m + ' on ' + klass.name + ' doesnt allow setting, make sure that it is property, not a method';
+					}
+				}
+			}
 
-		if (typeof (klass.prototype[m]) == 'function') {
+		} else if (typeof (klass.prototype[m]) == 'function') {
 			if (!excluded.includes(m)) {
 				methods[m] = klass.prototype[m];
 			}
@@ -165,54 +147,20 @@ export function bootstrap(klass: Constructor<Component>) {
 	}
 
 	proper.methods = methods;
-	proper.created = function () {
-		let injections = klass.prototype.injections;
-		if (injections) {
-			for (let i of injections) {
-				this[i] = klass.prototype[i];
-			}
-		}
-		try {
-			if (klass.prototype.created) {
-				klass.prototype.created.call(this);				
-			}
-		}
-		catch (e) {
-			debugger;
-			throw e;
-		}
+	if (klass.prototype.created) {
+		proper.created = klass.prototype.created;
 	}
-
-	if (klass.prototype.computed) {
-		proper.computed = proper.computed || {};
-		for (let c of klass.prototype.computed) {
-			let desc : any = Object.getOwnPropertyDescriptor(c.target, c.propertyKey);
-			proper.computed[c.propertyKey] = {
-				get() {
-
-					return desc.get.call(this);
-				},
-				set(value : any) {
-
-					if (!desc.set) {
-						throw 'Property ' + c.propertyKey + ' on ' + name + ' doesnt allow setting, make sure that it is property, not a method';
-					}
-					desc.set.call(this, value);
-				}
-			}
-		}
-	}
-
+	
 	if (klass.prototype.watches) {
 		proper.watch = proper.watch || {};
-		let toWatchList = proto.watches.multi((x : any) => x.name);
+		let toWatchList = proto.watches.multi((x: any) => x.name);
 		for (let toWatch of toWatchList) {
 			let handler = {
-				handler: function (n : any, o: any) {
-					let handlers = proto.watches.filter((x : any) => x.name.includes(toWatch)).map((x : any) => x.propertyKey);
+				handler: function (n: any, o: any) {
+					let handlers = proto.watches.filter((x: any) => x.name.includes(toWatch)).map((x: any) => x.propertyKey);
 					for (let index = 0; index < handlers.length; index++) {
-						const handler = handlers[index];						
-						klass.prototype[handler].call(this, n, o, toWatch);						
+						const handler = handlers[index];
+						klass.prototype[handler].call(this, n, o, toWatch);
 					}
 				},
 				deep: true,
@@ -223,7 +171,7 @@ export function bootstrap(klass: Constructor<Component>) {
 	}
 
 	proper.beforeCreate = klass.prototype.beforeCreate;
-	proper.mounted = klass.prototype.mounted;	
+	proper.mounted = klass.prototype.mounted;
 	proper.updated = klass.prototype.updated;
 	proper.beforeUpdate = klass.prototype.beforeUpdate;
 	proper.beforeMount = klass.prototype.beforeMount;
